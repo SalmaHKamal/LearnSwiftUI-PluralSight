e8315f30e8c9c284d5ee2a64d284646057cbcf62
Salma Hassan <salmahassan087@gmail.com>
2022-06-27 07:57:10 +0200

    Initial Commit

---
+++ LearnSwiftUI-PluralSight/LearnSwiftUI_PluralSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUI_PluralSightApp.swift
+//  LearnSwiftUI-PluralSight
+//
+//  Created by Salma Hassan on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUI_PluralSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
